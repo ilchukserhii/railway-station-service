ce8add496c0a9098bbfd79ddcd063e1436218462
Seroga <ilchuk.cergey@gmail.com>
2026-05-16 00:32:04 +0300

    Regicter admin models and add temporary API data

--- railway/admin.py
+++ railway/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from railway.models import (
+    Route,
+    Station,
+    Crew,
+    Trip,
+    Train,
+    TrainType,
+    Ticket,
+    Order
+)
 
-# Register your models here.
+
+class TicketInline(admin.TabularInline):
+    model = Ticket
+    extra = 1
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [TicketInline,]
+
+
+admin.site.register(Train)
+admin.site.register(TrainType)
+admin.site.register(Route)
+admin.site.register(Station)
+admin.site.register(Crew)
+admin.site.register(Trip)
+admin.site.register(Ticket)
