3b12e30e70c8f930d861fe17738744d4f9ce8d39
Seroga <ilchuk.cergey@gmail.com>
2026-05-19 14:22:13 +0300

    Implement Simple JWT settings, add user URLs and create User model migrations

--- railway_station_service/settings.py
+++ railway_station_service/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -131,6 +131,13 @@ REST_FRAMEWORK = {
         "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 5,
 
-    "DEFAULT_AUTHENTICATION_CLASSES":
+    "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFE_TIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFE_TIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKEN": False,
 }
--- railway_station_service/urls.py
+++ railway_station_service/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/railway/", include("railway.urls", namespace="railway")),
+    path("api/user/", include("user.urls", namespace="user")),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
