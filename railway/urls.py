3325583082e26846769a1a96717b5c946a22fdf0
Seroga <ilchuk.cergey@gmail.com>
2026-05-20 18:53:14 +0300

    Fix flake8 errors

--- railway/urls.py
+++ railway/urls.py
@@ -1,8 +1,15 @@
-from django.urls import path,include
+from django.urls import path, include
 from rest_framework import routers
 
-from railway.views import TripViewSet, OrderViewSet, RouteViewSet, TrainViewSet, TrainTypeViewSet, CrewViewSet, \
+from railway.views import (
+    TripViewSet,
+    OrderViewSet,
+    RouteViewSet,
+    TrainViewSet,
+    TrainTypeViewSet,
+    CrewViewSet,
     StationViewSet
+)
 
 app_name = "railway"
 
